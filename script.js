01cb3fc2582ba62541ce88af9111d9004ddf8a8b
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-28 23:55:36 +0200

    task 2

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-// task 1
+'use strict'
 
-function getData (url){
-   let result = fetch(url);
-   let title = [];
-   result.then(response => {
-            if(response.ok){
-               return response.json();
-            }
-            throw new Error(`Error: ${response.status}`);
-         })
-         .then((data) => {
-         for(let item of data){
+// // task 1
+
+// function getData (url){
+//    let result = fetch(url);
+//    let title = [];
+//    result.then(response => {
+//             if(response.ok){
+//                return response.json();
+//             }
+//             throw new Error(`Error: ${response.status}`);
+//          })
+      //    .then((data) => {
+      //    for(let item of data){
+      //       if(item.title[0]==='a'){
+      //          title.push(item);
+      //       }
+      //    }
+      // });
+//       console.log(title);
+//    };
+// getData('https://jsonplaceholder.typicode.com/todos');
+
+// task 2
+
+async function getAsyncData (url){
+   try{
+      let result = await fetch(url);
+   if(result.ok){
+      let response = await result.json();
+         console.log(response);
+         let title = [];
+         for(let item of response){
             if(item.title[0]==='a'){
                title.push(item);
             }
          }
-      });
-      console.log(title);
+         console.log(title);
+      }
+   }
+      catch(error){
+         console.log(error.message);
+      }
    };
-getData('https://jsonplaceholder.typicode.com/todos');
-
-// task 2
+getAsyncData('https://jsonplaceholder.typicode.com/todos');

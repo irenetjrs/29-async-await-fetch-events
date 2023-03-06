95ce9050273ea26f67e1daf0c6c7f096ed788b09
irenetjrs <irkapidgorodetska@gmail.com>
2023-03-06 15:02:58 +0200

    task 4

--- script.js
+++ script.js
@@ -73,3 +73,48 @@ async function getAsyncData(url){
       }
    };
 getAsyncData('https://jsonplaceholder.typicode.com/todos');
+
+// task 4
+
+const btn1 = document.querySelector('.add-a');
+const btn2 = document.querySelector('.add-ab');
+const btn3 = document.querySelector('.add-a');
+console.log(btn1);
+async function getAsyncData1(url){
+   try{
+   let result = await fetch(url);
+   if(result.ok){
+      let response = await result.json();
+         let filterA = response.filter((item) => item.title[0]==='a');
+            
+         btn1.addEventListener('click', aList);
+            
+            function aList(){
+               filterA.forEach((elem) => {
+               let divA = document.createElement('div');
+               divA.textContent = JSON.stringify(elem);
+               userA.append(divA);
+            });
+            }
+
+            btn2.addEventListener('click', abList);
+            
+            function abList(){
+            let filterAB = filterA.filter((item) => item.title[0]==='a' && item.title[1]==='b');
+            filterAB.forEach((elem) => {
+               let divAB = document.createElement('div');
+               divAB.textContent = JSON.stringify(elem);
+               userAB.append(divAB);
+            });
+         }
+            btn3.addEventListener('click', all);
+            function all(){
+               aList();
+               abList();
+            }
+      }
+   }catch(error){
+         console.log(error.message);
+      }
+   }
+getAsyncData1('https://jsonplaceholder.typicode.com/todos');
